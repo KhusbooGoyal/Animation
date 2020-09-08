dd50e977464700403ef00fcb7b43a698fba9dd22
Khusboo <khusboo1299@gmail.com>
2020-09-08 18:08:46 +0530

    Frame Animation

---
+++ app/src/main/java/com/example/frameanimation/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.frameanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private AnimationDrawable batAnimation;
+    private ImageView batImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batImage = (ImageView) findViewById(R.id.batID);
+        batImage.setBackgroundResource(R.drawable.bat_anim);
+        batAnimation = (AnimationDrawable) batImage.getBackground();
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        batAnimation.start();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //stop the animation
+                batAnimation.stop();
+            }
+        } , 5000); //5seconds
+        return super.onTouchEvent(event);
+    }
+}
